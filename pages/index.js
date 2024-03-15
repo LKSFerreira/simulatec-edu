0b9141cac0bd6bc932db4c47806fa3ee1ca4d157
Lucas Ferreira <lks_ferreira@hotmail.com>
2024-03-15 11:54:43 -0300

    :sparkles: Implementado coleta de métricas da vercel

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,12 @@
 import Script from "next/script";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 
 function Home() {
   return (
     <div>
       <script src="https://cdnjs.cloudflare.com/ajax/libs/p5.js/1.7.0/p5.min.js"></script>
       <Script src="/rain.js" strategy="beforeInteractive" />
+      <SpeedInsights />
     </div>
   );
 }
